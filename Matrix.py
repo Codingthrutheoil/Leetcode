0a5c15359947f640ce28690e5ecb2e1f23f1f6be
Codingthrutheoil <145480149+Codingthrutheoil@users.noreply.github.com>
2023-12-13 22:32:11 +0800

    Add files via upload

---
+++ Matrix.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def numSpecial(self, mat):
+        m = len(mat)
+        n = len(mat[0])
+        row_sum = [0] * m
+        col_sum = [0] * n
+        count = 0
+
+        for i in range(m):
+            for j in range(n):
+                row_sum[i] += mat[i][j]
+                col_sum[j] += mat[i][j]
+
+        for i in range(m):
+            for j in range(n):
+                if mat[i][j] == 1 and row_sum[i] == 1 and col_sum[j] == 1:
+                    count += 1
+
+        return count
