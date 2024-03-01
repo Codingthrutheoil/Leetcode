e4e31eb7f23caa54823a62f1890df99d7c4b4d5e
Codingthrutheoil <145480149+Codingthrutheoil@users.noreply.github.com>
2024-03-02 06:24:32 +0800

    Add files via upload

---
+++ Maximum Odd Binary Number.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def maximumOddBinaryNumber(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return ((s.count("1") - 1)* "1") + s.count("0")*"0" + "1"
