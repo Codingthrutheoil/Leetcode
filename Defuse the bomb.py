1ace6461f8a707c5540bf29f360f167f4783eac9
Codingthrutheoil <145480149+Codingthrutheoil@users.noreply.github.com>
2024-11-18 22:30:51 +0800

    Update Defuse the bomb.py

--- Defuse the bomb.py
+++ Defuse the bomb.py
@@ -1,16 +1,3 @@
-def iterOfK(startnumber, kvalue, length):
-    if kvalue < 0:
-        kvalue = abs(kvalue)
-        iter_of_k = [startnumber - 1 if startnumber not in [0, length] else length - 1]
-        for a in range(1, kvalue):
-            iter_of_k.append((iter_of_k[0] - a) % length)
-        return iter_of_k
-    iter_of_k = [(startnumber + 1) % length]
-    for a in range(1, kvalue-1):
-        iter_of_k.append((iter_of_k[0]+a) % length)
-    iter_of_k.append((iter_of_k[-1] + 1) % length)
-    return iter_of_k
-
 class Solution(object):
     def decrypt(self, code, k):
         """
@@ -18,17 +5,20 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
-        code_return = [0 for a in code]
-        if k == 0:
-            return code_return
-        if k == 1:
-            for a in range(len(code)-1):
-                code_return[a] = code[a+1]
-            code_return[-1] = code[0]
-            return code_return
-        len_of_k = len(code)
-        for a in range(len(code)):
-            iter_of_k = iterOfK(a, k, len_of_k)
-            for b in iter_of_k:
-                code_return[a] = code_return[a] + code[b]
-        return code_return 
+        code3 = []
+        p = len(code)
+        if k > 0:
+            for a in range(p):
+                code4 = 0
+                for b in range(abs(k)):
+                    code4 += code[(b+a+1)%p]
+                code3.append(code4)
+        elif k < 0:
+            for a in range(p):
+                code4 = 0
+                for b in range(abs(k),0,-1):
+                    code4 += code[a-b]
+                code3.append(code4)
+        else:
+            return([0 for a in range(p)])
+        return(code3)
